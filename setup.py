25b55ebcdbf591eb08c511edb7340029ab6a2ad8
Marika Buchholz <marikabuchholz@gmail.com>
2016-07-14 15:32:53 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,11 +3,8 @@ import sys
 import numpy
  
 nproc = [1, 2, 4, 8, 16, 32]
-dt = 0.5
-nsteps = 1000
-prefix = 'graphene'
 
-def tddft_sbatch(np): 
+def tddft_sbatch(np, pw_path, tddft_path): 
     string = """#!/bin/bash                                                                                      
 #SBATCH --job-name=long-run-"""
     string+= str(np)
@@ -16,19 +13,28 @@ def tddft_sbatch(np):
 #SBATCH --error=cn-tddft.err                                                                    
 #SBATCH --time=00:10:00                                                                         
 #SBATCH --qos=normal                                                                            
-#SBATCH --nodes=8                                                                               
+#SBATCH --nodes="""
+    string+=str(np)
+    string+="""
 #SBATCH --mem=4000                                                                              
 #SBATCH --ntasks-per-node=16                                                                    
 ##SBATCH --mpi=pmi2
                                                                              
-PREFIX=graphene
-NP="""
+"""
     string+=str(np)
     string+="""
 
 rm -rf *out
-mpirun -np $NP $PW_PATHpw.x < $PREFIX.pw-in > $PREFIX.pw-out
-mpirun -np $NP $TDDFT_PATHtddft.x < $PREFIX.tddft-in > $PREFIX.tddft-out
+mpirun -np """ 
+    string+=str(np)
+    string+=""" """
+    string+=pw_path
+    string+=""" < ~/graphene/graphene.pw-in > ~/graphene/graphene.pw-out
+mpirun -np """
+    string+=str(np)
+    string+=""" """
+    string+=tddft_path
+    string+=""" < ~/graphene/graphene.tddft-in > ~/graphene/graphene.tddft-out
 """
     return string
 
@@ -37,33 +43,37 @@ if change_path == "1":
     pw_path = raw_input("New pw path: ")
     tddft_path = raw_input("New tddft path: ")
 else:
-    pw_path = '/home/marikab/espresso-5.2.0/PW/src/'
-    tddft_path = '/home/marikab/ce-tddft-master/bin/'
+    pw_path = '/home/marikab/espresso-5.2.0/PW/src/pw.x'
+    tddft_path = '/home/marikab/ce-tddft-master/bin/tddft.x'
+
+#removes procdirs from previous runs
+all_procs = [1, 2, 4, 8, 16, 32]
+for np in all_procs:
+    procdir = str(np) + 'procs'
+    if os.path.exists(procdir):
+        os.system('rm -r ' + procdir)
 
 for np in nproc:        
     procdir = str(np) + 'procs'
-    if not os.path.exists(procdir):
-        os.system('mkdir ' + procdir) #makes a directory for number of processors 
-    else:
-        os.system('rm -r ' + procdir)
-        print "removed"
-        os.system('mkdir ' + procdir) 
-    os.system('cp ./graphene/graphene.pw-in procdir') #copies pw-in file to procdir
+    os.system('mkdir ' + procdir) #makes a directory for number of processors  
+    os.system('cp ~/graphene/graphene.pw-in procdir') #copies pw-in file to procdir
     os.system('cd ~/' + procdir)
-    method = '16atoms' + str(np) + 'proc'
-    f = open(method + '.txt', "w") 
-    f.write(tddft_sbatch(np))
 
-    os.system('mv ' + method + '.txt' + ' ' + '~/' + procdir)
-    f.close()
-    tps = 0.25 #time per solve
-    timehours = min(48, int(numpy.ceil(tps*nsteps/3600.)))
+    os.system('cp ' + '~/graphene/graphene.pw-in ' + procdir + '/graphene.pw-in')
+    os.system('cp ' + '~/graphene/graphene.tddft-in ' + procdir + '/graphene.tddft-in')
 
     fname = procdir + '/tddft.sbatch'
-    os.system('cp /home/marikab/graphene/tddft.sbatch ~/' + fname)
+    if os.path.isfile('~/' + procdir + '/tddft.sbatch*'):
+       os.system('rm -r ' + fname)
+    
+    f = open(fname, "w")
+    f.write(tddft_sbatch(np, pw_path, tddft_path))
     os.system('chmod u+x ' + procdir + ' ~/' + fname) 
     os.system('cat /home/marikab/graphene/graphene.pw-out | grep PWSCF | awk \'/PWSCF/{i++}i==3{print $3}\'')
+    f.close()
 
+for np in nproc:
+    os.system('sbatch ' + str(np) + 'procs/tddft.sbatch')
 
 
 
